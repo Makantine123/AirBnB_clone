59ae98e61a25683a5fb62c74fec2a3db4162f3e2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-08 14:31:59 +0200

    Commit

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -18,6 +18,13 @@ class Test_BaseModel(unittest.TestCase):
         self.base1.name = "Siphelele"
         self.base1.my_number = 34
 
+    def test_doc(self):
+        self.assertNotEqual(len(BaseModel.__doc__), 0)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
     def test_init(self):
         self.assertTrue(isinstance(self.base1, BaseModel))
 
@@ -41,6 +48,24 @@ class Test_BaseModel(unittest.TestCase):
         self.base1.save()
         self.assertEqual(self.base1.created_at, beforeCreated_at)
         self.assertNotEqual(self.base1.updated_at, beforeUpdated_at)
+
+    def test_instance_creation_kwargs(self):
+        "Kwargs dictionary"
+        mydict = {"id": "1111111-11111-11111-11111-11111",
+                   "created_at": "2023-03-28T21:03:54.053212",
+                   "__class__": "BaseModel",
+                   "updated_at": "2023-03-28T21:03:54.057521"}
+        base2 = BaseModel(**mydict)
+        # Check if attributes exists
+        self.assertTrue(hasattr(base2, "id"))
+        self.assertTrue(hasattr(base2, "created_at"))
+        self.assertTrue(hasattr(base2, "updated_at"))
+        self.assertTrue(hasattr(base2, "__class__"))
+        self.assertTrue(base2.__class__ not in base2.__dict__)
+        # Check DateTime formats and Values
+        self.assertEqual(base2.id, "1111111-11111-11111-11111-11111")
+        self.assertEqual(base2.created_at.isoformat(), "2023-03-28T21:03:54.053212")
+        self.assertEqual(base2.updated_at.isoformat(), "2023-03-28T21:03:54.057521")
 
 if __name__ == "__main__":
     unittest.main()
